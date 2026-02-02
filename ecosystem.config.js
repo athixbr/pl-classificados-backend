1e23202953f8ef876b1199580fbae6eb4d328281
athix <35705895+athixbr@users.noreply.github.com>
2026-02-02 16:41:29 -0300

    Update: Alterar porta do servidor para 3004

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,11 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development',
-      PORT: 3003
+      PORT: 3004
     },
     env_production: {
       NODE_ENV: 'production',
-      PORT: 3003
+      PORT: 3004
     },
     error_file: './logs/err.log',
     out_file: './logs/out.log',
